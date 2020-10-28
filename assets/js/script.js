f5304de6507fcee07172a13e48fc37dc0ca7c9f9
Ph0enixT1m3 <diman_hawaiki2@mail.ru>
2020-10-28 23:55:59 +0300

    feat: сделал десктоп-секции до "принципов"

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$('.main').slick({
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    prevArrow: $('.main__control-left'),
+    nextArrow: $('.main__control-right')
+  });
+$('.principles__slider__item').slick({
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    prevArrow: $('.principles__control-left'),
+    nextArrow: $('.principles__control-right')
+  });
