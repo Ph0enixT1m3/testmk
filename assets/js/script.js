f341346bf7b8c23ee8e14bf7470fcbae2f9f7629
Ph0enixT1m3 <diman_hawaiki2@mail.ru>
2020-10-30 01:52:56 +0300

    feat: полностью закончил десктопную версию сайта

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,17 @@ $('.principles__slider__item').slick({
     prevArrow: $('.principles__control-left'),
     nextArrow: $('.principles__control-right')
   });
+
+$('.reviews__list').slick({
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    dots: true,
+    prevArrow: $('.reviews__list__control__arrows-left'),
+    nextArrow: $('.reviews__list__control__arrows-right')
+  });
+
+  //1. После загрузки страницы,  когда все элементы будут доступны выполнить...
+$(function(){
+  //2. Получить элемент, к которому необходимо добавить маску
+  $("#phone").mask("+375 (99) 999-99-99", {placeholder: "X" });
+});
