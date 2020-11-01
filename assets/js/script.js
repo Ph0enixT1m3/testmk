4b138acc46a30f30d7ec9e3189f8f237b85b342d
Ph0enixT1m3 <diman_hawaiki2@mail.ru>
2020-11-01 20:54:15 +0300

    feat: закончил адаптив, добавил кнопку в моб. версии "наверх"

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,3 +36,36 @@ $(function(){
   //2. Получить элемент, к которому необходимо добавить маску
   $("#phone").mask("+375 (99) 999-99-99", {placeholder: "X" });
 });
+
+$(document).ready(function(){
+  $('.header__mobile__burger').click(function(event){
+    $('.header__mobile__burger').toggleClass('header__mobile__burger-active');
+    $('body').toggleClass('lock');
+  })
+});
+$(document).ready(function(){
+  $('.header__mobile__burger').click(function(event){
+    $('.header__mobile-menu').toggleClass('header__mobile-menu-active');
+  })
+});
+
+// Кнопка «Наверх/Вниз»
+var lastScrollPosition = 0; 
+
+$('#scroll-up').click( function(){
+	if ( $(document).scrollTop() > 0 ) {
+		$('html, body').animate({scrollTop:0},1000);
+		lastScrollPosition = $(document).scrollTop();
+	} else {
+		$('html, body').animate({scrollTop:lastScrollPosition},1000);
+	}	
+});
+
+$(document).scroll( function() {
+	if ( $(document).scrollTop() > 0 ) {
+		$('#scroll-up').fadeIn();
+		// $('#scroll-up').text('Наверх');
+	} else {
+		$('#scroll-up').addClass('btn-top-active');
+	}
+});
